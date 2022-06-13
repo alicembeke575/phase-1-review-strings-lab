81984e9cc60ff15a1a8e9c1d9295510d286b7e17
Alice Mukulu <alice.mukulu@student.moringaschool.com>
2022-06-13 17:36:47 +0300

    commit tree 6096315778a3e9e7166d74147e536bb1c0e0ef92

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'Alice ';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+'Alice'[0];
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
